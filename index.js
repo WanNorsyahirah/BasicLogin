e3a0fe45936fc0d69b58395be9a1de6a82665b6f
WanNorsyahirah <b022110103@student.utem.edu.my>
2023-03-31 09:34:33 +0800

    register function done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let dbUser =[
     {
         username: "wanno",
-        password: " password",
+        password: "password",
         name: "wanno bijak",
         email: "wanno@utem.edu.my"
     },
@@ -29,9 +29,21 @@ function login(username, password){
 else {
     return " username not found"
 } }
+function register(newusername, newpassword, newname, newemail) {
+    // To do : check if usrname exist
+
+    dbUser.push( {
+        username: newusername,
+        password: newpassword,
+        name: newname,
+        email: newemail
+    })
+}
 
 // try to call login function
 //console.login("wanno", "password")
 //console.login("wanno", "12345")
-console.log(login ("wanno","34567"))
-
+console.log(login ("wanno","password"))
+console.log(login("wanno", "password"))
+register("bb","blablabla", "pee-mak", "gocoh@gmail.com")
+console.log(login("bb", "blablabla"))
